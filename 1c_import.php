a2020322b079b2f3a784567cf8fb4f56bbfe0d85
Igor Achkasov <a.i.n.1989@mail.ru>
2018-09-25 20:32:00 +0300

    delete space

--- 1c_import.php
+++ 1c_import.php
@@ -9,7 +9,6 @@ header('Cache-Control: no-cache, must-revalidate');
 require_once $_SERVER['DOCUMENT_ROOT']."/B24Entity/CommandRoute.php";
 require_once $_SERVER["DOCUMENT_ROOT"]."/B24Entity/JsonResponse.php";
 
-
 /**
   логировать запросы и ошибки заказов
 */
